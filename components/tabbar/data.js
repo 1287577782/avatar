e342d4e7492bf04ae8e3fe70cc329fbcdbde7886
1287577782 <1287577782@qq.com>
2023-11-05 16:34:19 +0800

    自定义tabbar

---
+++ components/tabbar/data.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    icon: 'home',
+    text: '首页',
+    url: 'pages/home/index',
+  },
+  {
+    icon: 'sort',
+    text: '分类',
+    url: 'pages/logs/logs',
+  },
+  {
+    icon: 'person',
+    text: '个人中心',
+    url: 'pages/usercenter/index',
+  },
+];
