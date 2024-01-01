c20212dd44930135dc0aaef2bce784245fa62987
1287577782 <1287577782@qq.com>
2024-01-01 15:35:53 +0800

    test

--- pages/home/index.js
+++ pages/home/index.js
@@ -5,33 +5,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-    // imgSrcs: ["/images/1.jpg",'/images/2.jpg','/images/3.jpg'],
-    tabList: [],
     channel: [],
-    menu:   [{"name":"鹊桥", "icon":""}, {"name":"安居", "icon":""}, {"name":"招聘","icon":""}, {"name":"家政", "icon":""}],
     "persionList":[{"id":"test1", "img":"/images/1.jpg", "username":"test","disp":"", "hight":175}, 
     {"id":"test2", "img":"/images/2.jpg", "username":"test2","disp":"", "hight":165},
     {"id":"test2", "img":"/images/2.jpg", "username":"test2","disp":"", "hight":165},
     {"id":"test2", "img":"/images/2.jpg", "username":"test2","disp":"", "hight":165},
     {"id":"test2", "img":"/images/2.jpg", "username":"test2","disp":"", "hight":165},
     {"id":"test2", "img":"/images/2.jpg", "username":"test2","disp":"", "hight":165},
-    {"id":"test2", "img":"/images/2.jpg", "username":"test2","disp":"", "hight":165},
-    {"id":"test2", "img":"/images/2.jpg", "username":"test2","disp":"", "hight":165},
-    {"id":"test2", "img":"/images/2.jpg", "username":"test2","disp":"", "hight":165},
-    {"id":"test2", "img":"/images/2.jpg", "username":"test2","disp":"", "hight":165},
-    {"id":"test2", "img":"/images/2.jpg", "username":"test2","disp":"", "hight":165},
-    {"id":"test2", "img":"/images/2.jpg", "username":"test2","disp":"", "hight":165},
-    {"id":"test2", "img":"/images/2.jpg", "username":"test2","disp":"", "hight":165},
-    {"id":"test2", "img":"/images/2.jpg", "username":"test2","disp":"", "hight":165},
-    {"id":"test2", "img":"/images/2.jpg", "username":"test2","disp":"", "hight":165},
-    {"id":"test3", "img":"/images/3.jpg", "username":"test3", "disp":"", "hight":163}],
-    pageLoading: false,
-    current: 1,
-    autoplay: true,
-    duration: '500',
-    interval: 5000,
-    navigation: { type: 'dots-bar' },
-    swiperImageProps: { mode: 'scaleToFill' },
+    {"id":"test2", "img":"/images/2.jpg", "username":"test2","disp":"", "hight":165}]
   },
 
   /**
