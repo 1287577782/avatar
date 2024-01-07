278f44e64529d12abc8cd241139188c167564cfe
10184530 <10184530@ad.zte.com>
2024-01-07 17:27:08 +0800

    修改自我介绍

--- pages/home/index.js
+++ pages/home/index.js
@@ -5,12 +5,12 @@ Page({
    * 页面的初始数据
    */
   data: {
-    "persionList":[{"id":"test1", "img":"/icon/man.png",  "gender":"man",  "username":"test",  "disp":"", "hight":175, "picture":[
+    "persionList":[{"id":"test0", "img":"/icon/man.png",  "gender":"man",  "username":"子非鱼乐",  "introduce":"【黄桥征婚】89年勤丰村小哥哥，身高175，体重140斤，大专学历，国企非编，年收入10W，工作稳定，希望找一位89-95年之间，身高160以上的女孩。", "hight":175, "picture":[
       {"url":"/images/t1.jpg"}, {"url":"/images/t2.jpg"},{"url":"/images/t3.jpg"}
     ]}, 
-                  {"id":"test2",  "img":"/icon/women.png", "gender":"women", "username":"test2", "disp":"", "hight":165},
-                  {"id":"test2",  "img":"/icon/women.png","gender":"women",  "username":"test2", "disp":"", "hight":165},
-                  {"id":"test2",   "img":"/icon/man.png", "gender":"man",  "username":"test2", "disp":"", "hight":165}]
+                  {"id":"test1",  "img":"/icon/women.png", "gender":"women", "username":"云上之约", "introduce":"【黄桥征婚】96年刘家堡村小姐姐，身高165，体重96斤，本科学历，小学老师，年收入10W，性格温和，喜欢瑜伽，希望找一位情绪稳定，热爱生活的男孩。", "hight":165},
+                  {"id":"test2",  "img":"/icon/women.png","gender":"women",  "username":"简单", "introduce":"【黄桥征婚】98合兴村小姐姐，身高163，体重106斤，黄桥镇有房，本科学历，初中在编老师，容貌清秀，无恋爱史。希望找一位身体健康，真诚善良，做事靠谱的男孩。", "hight":163},
+                  {"id":"test3",   "img":"/icon/man.png", "gender":"man",  "username":"流年", "introduce":"【黄桥征婚】95年泰兴小哥哥，身高168，有房有车，本科学历，事业单位工作，年收入17W，阳光开朗，有上进心。希望找一位工作稳定，真诚靠谱的女孩。", "hight":168}]
   }, 
 
   /**
